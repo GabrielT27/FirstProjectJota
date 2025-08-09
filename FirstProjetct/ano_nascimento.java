14235e42446030f14de423d443774ed2e886b45d
Gabriel Marques <gabrielmarquesterr@gmail.com>
2025-08-08 21:49:39 -0300

    Add files via upload

---
+++ FirstProjetct/ano_nascimento.java
@@ -0,0 +1,9 @@
+public class ano_nascimento {
+    public static void main(String args[]) {
+        int a =19;
+        int b =2025;
+        int subtracao = b - a;
+        var cidade = "Limeira";
+        System.out.println("O ano do meu nascimento é: " + subtracao + ", E a cidade na qual nasci é: " + cidade);
+    }
+}
