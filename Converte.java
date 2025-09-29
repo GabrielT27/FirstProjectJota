da06f18bbe7ec34418642d57c9d2aa4297ce789e
Gabriel Marques <gabrielmarquesterr@gmail.com>
2025-09-29 16:53:47 -0300

    Print dos resultados.

--- Converte.java
+++ Converte.java
@@ -4,10 +4,8 @@ class Converte {
         ConversorUnidade conv = new ConversorUnidade();
         System.out.println("20 pés são=" + conv.pes2cm(20) + "centímetros");
         System.out.println("5 polegadas são=" + conv.pol2cm(5) + "centímetros");
-        
-        System.out.println("20 pés são= " + ConversorUnidade.pes2cm(20)+"centímetros");
-        
-        System.out.println("5 polegadas são="+ConversorUnidade.pol2cm(5)+"centímetros");
+        System.out.println("3  milimetros são=" + conv.mm2cm(3) + "centímetros");
+        System.out.println("10 metros são=" + conv.m2cm(10) + "centímetros");
         
         
     }
