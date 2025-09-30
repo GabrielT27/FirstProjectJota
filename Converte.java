2289e49b643b3d7c7bd9e25e3f843a3c6aecd2c0
Gabriel Marques <gabrielmarquesterr@gmail.com>
2025-09-30 16:48:52 -0300

    Update ConverteMain.java
    Main do Conversor feito e finalizado.

--- Converte.java
+++ Converte.java
@@ -1,12 +1,64 @@
-class Converte {
+import java.util.Scanner;
+
+class ConverteMain {
     public static void main(String args[])
     {
-        ConversorUnidade conv = new ConversorUnidade();
-        System.out.println("20 pés são=" + conv.pes2cm(20) + "centímetros");
-        System.out.println("5 polegadas são=" + conv.pol2cm(5) + "centímetros");
-        System.out.println("3  milimetros são=" + conv.mm2cm(3) + "centímetros");
-        System.out.println("10 metros são=" + conv.m2cm(10) + "centímetros");
+        Scanner tst = new Scanner(System.in);
         
+        System.out.println("Digite seu nome: ");
+        String nome = tst.nextLine();
+        
+        
+        ConversorUnidade conv = new ConversorUnidade();
+        
+        
+        System.out.println("Digite um número em pés para converter em centímetros: ");
+        double valorPes = tst.nextDouble();
+        
+        double convertidoPes = conv.pes2cm(valorPes);
+        
+        System.out.println(valorPes + " Pés, Convertido para centímetros é igual a:" + convertidoPes);
+        
+        
+        
+        
+        System.out.println(" Escreva um número em polegadas para converter em centímetros: ");
+        double valorPol = tst.nextDouble();
+        
+        double convertidoPol = conv.pol2cm(valorPol);
+        
+        System.out.println(valorPol + " Polegadas, Convertido para centímetros é igual a: " + convertidoPol);
+        
+        
+        
+        System.out.println(" Escreva um número em milimetros para converter em centímetros: ");
+        double valorMM = tst.nextDouble();
+        
+        double convertidoMM = conv.mm2cm(valorMM);
+        
+        System.out.println(valorMM + " Milimetros. é igual a " + convertidoMM + " em centímetros"); 
+        
+        
+        System.out.println(" Escreva um número em metros para converter em centímetros: ");
+        double valorM = tst.nextDouble();
+        
+        double convertidoM = conv.m2cm(valorM);
+        
+        System.out.println(valorM + " Metros. É igual a " + convertidoM + " em centímetros");
+        
+        
+        System.out.println("Escreva um número e jarda para converter em centímetros:");
+        
+        double valorJ = tst.nextDouble();
+        double convertidoJ = conv.j2cm(valorJ);
+        
+        System.out.println(valorJ + " Jardas. É igual a " + convertidoJ + " em centímetros.");
+        
+        
+        System.out.println("Obrigado por confiar em nós para realizar as suas conversões!!!");
+        
+        
+        tst.close(); // sempre bom fechar o Scanner
         
     }
 }
